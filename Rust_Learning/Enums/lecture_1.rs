a224406ec3cd077619a8637c4dabd0f95e518266
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-01-06 21:01:53 +0600

    Update lecture_1.rs

--- Rust_Learning/Enums/lecture_1.rs
+++ Rust_Learning/Enums/lecture_1.rs
@@ -1,1 +1,23 @@
 
+#[derive(Debug)]
+enum CardSuit {
+    Hearts,
+    Diamonds,
+    Spades,
+    Clubs,
+}
+
+struct Card {
+    rank: String,
+    suit: CardSuit,
+}
+
+fn main() {
+    let first_card = CardSuit::Hearts;
+    let mut second_card = CardSuit::Spades;
+    second_card = CardSuit::Clubs;
+    println!("{:?}", second_card);
+
+    let card_suits = [CardSuit::Hearts, CardSuit::Clubs];
+    let card_suits = (CardSuit::Hearts, CardSuit::Spades);
+}
