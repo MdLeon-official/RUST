bec8afedf660d8086a13dbc8e18bfb06b172fbc4
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-01-07 00:53:40 +0600

    Update lecture_2.rs

--- Rust_Learning/Enums/lecture_2.rs
+++ Rust_Learning/Enums/lecture_2.rs
@@ -1,1 +1,15 @@
+#[derive(Debug)]
+
+enum PaymentMethodType {
+    CreditCard(String),
+    DebitCard(String),
+    PayPal,
+}
+
+fn main() {
+    let visa = PaymentMethodType::CreditCard(String::from("1232-5464"));
+    let mastercard = PaymentMethodType::DebitCard(String::from("1232-546-3254-3423"));
+
+    println!("VISA: {:?} and MASTERCARD: {:?}", visa, mastercard)
+}
 
