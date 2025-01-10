b800e7e038a6d8255187b6009f29e31d682b2d05
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-01-10 15:34:21 +0600

    Create basic_data_types.rs

---
+++ CodeForces-with-RUST/ ICPC Assiut University Community /Sheet #1 (Data type - Conditions)/basic_data_types.rs
@@ -0,0 +1,18 @@
+    use std::io;
+     
+    fn main() {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to get user input");
+     
+        let values: Vec<String> = input
+            .trim()
+            .split_whitespace()
+            .map(|x| x.to_string())
+            .collect();
+     
+        for value in values {
+            println!("{}", value);
+        }
+    }
