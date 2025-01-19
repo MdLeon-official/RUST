f1e4758124b7fb00a8c2187a7441f04fa753729a
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-01-19 17:08:41 +0600

    Create lecture_5.rs

---
+++ Rust_Learning/Option and result enum/lecture_5.rs
@@ -0,0 +1,19 @@
+fn is_item_in_stock(item_is_in_system: bool, item_is_in_stock: bool) -> Option<bool> {
+    if item_is_in_system && item_is_in_stock {
+        Option::Some(true)
+    } else if item_is_in_system {
+        Option::Some(false)
+    } else {
+        Option::None
+    }
+}
+
+fn main() {
+    let availability = is_item_in_stock(false, true);
+    // println!("{availability:?}")
+
+    match availability {
+        Option::Some(value) => println!("Item is available: {value}"),
+        Option::None => println!("Your item doesn't exist in out system."),
+    }
+}
