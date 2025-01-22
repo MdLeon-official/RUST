17985c53d771ba39527952698e3e2aee1de93e75
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-01-22 16:02:00 +0600

    Update lecture_12.rs

--- Rust_Learning/Option and result enum/lecture_12.rs
+++ Rust_Learning/Option and result enum/lecture_12.rs
@@ -1,1 +1,14 @@
+fn main() {
+    let result = divide(10.0, 0.0);
+    println!("{}",result.is_ok());
+    println!("{}",result.is_err());
+}
+
+fn divide(numerator: f64, denominator: f64) -> Result<f64, String> {
+    if denominator == 0.0 {
+        Err("Cannot divide by zero.".to_string())
+    } else {
+        Ok(numerator / denominator)
+    }
+}
 
