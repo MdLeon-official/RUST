fe856680ba4e1b55ccee6e7943bb5a26dc8894d5
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-01-26 16:21:33 +0600

    Create lecture_1.rs

---
+++ Rust_Learning/Vectors/lecture_1.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // let pizza_diameters: Vec<i32> = Vec::new();
+    // let pizza_diameters = Vec::<i32>::new();
+    let pizza_diameters = vec![5, 7, 34, 23, 4];
+    println!("{:?}", pizza_diameters);
+
+    let pastas: Vec<&str> = Vec::new();
+    println!("{:?}", pastas);
+}
