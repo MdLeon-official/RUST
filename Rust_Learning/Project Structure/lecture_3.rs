099de55a2a4d5629eb944cbefd1d9b0b63f62a80
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-02-02 20:41:09 +0600

    Create lecture_3.rs

---
+++ Rust_Learning/Project Structure/lecture_3.rs
@@ -0,0 +1,26 @@
+mod inventory {
+    const FLOOR_SPACE: i32 = 10000;
+    pub const MANAGER: &str = "Mine Inventory";
+
+    #[derive(Debug)]
+    enum ProductCategory {
+        Ladder,
+        Hammer,
+    }
+
+    #[derive(Debug)]
+    struct Item {
+        name: String,
+        category: ProductCategory,
+        quantity: u32,
+    }
+
+    fn talk_to_manager() {
+        println!("Hey, {MANAGER}, how's your coffee")
+    }
+}
+
+fn main() {
+    //println!("The manager of our inventory is {}", MANAGER);
+    println!("The manager of our inventory is {}", inventory::MANAGER);
+}
