9394879221048cf03e7646c75df178d51296a9d0
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-03-04 14:36:16 +0600

    Create Reversing.rs

---
+++ CodeForces-with-RUST/ ICPC-Assiut-University-Community/sheet-3_Arrays/Reversing.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+fn main() {
+    let mut iter = String::new();
+    io::stdin()
+        .read_line(&mut iter)
+        .expect("Failed to read line");
+    let mut user_sp = String::new();
+    io::stdin()
+        .read_line(&mut user_sp)
+        .expect("Failed to read line");
+    let mut number: Vec<i64> = user_sp
+        .trim()
+        .split_whitespace()
+        .map(|s| s.parse().expect("Failed to parse number"))
+        .collect();
+
+    number.reverse();
+    for num in &number {
+        print!("{} ", num);
+    }
+    print!("\n");
+}
